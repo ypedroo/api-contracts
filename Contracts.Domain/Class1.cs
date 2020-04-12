fd37f3e60e6bd358feb64a9a310f9f17436372e3
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-04-11 22:23:56 -0300

    Adds part model

--- Contracts.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Contracts.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ Contracts.Domain/Models/PartModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts.Domain.Models
+{
+    class PartModel
+    {
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
