70f4771ed442893fa2af864eacaa7f700155ba1c
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-04-11 22:38:46 -0300

    Adds command base

---
+++ Contracts.Domain.Core/Commands/CommandBase.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Contracts.Domain.Core.Commands
+{
+    public class CommandBase : Notifiable, IValidatable, IRequest<CommandResult>
+    {
+        public virtual void Validate() { }
+    }
+}
