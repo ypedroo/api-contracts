94b48e78ea03d86b4a5ecf1a0e29affb1d1e5966
ypedro <ynoa.pedro@outlook.com>
2020-08-15 18:05:21 -0300

    Add part entity

---
+++ Contracts.Domain/Models/Contract.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Contracts.Domain.Models
+{
+    public sealed class Contract
+    {
+        public byte[] File { get; set; }
+        public DateTime RequestedAt { get; set; }
+        public int Part { get; set; }
+    }
+}
