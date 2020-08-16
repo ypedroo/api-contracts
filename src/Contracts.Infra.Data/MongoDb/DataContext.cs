99d290cd8acf67ddcb7ada979cb42d79604b918a
ypedro <ynoa.pedro@outlook.com>
2020-08-16 12:13:52 -0300

    Add scractch of connection class using mongo driver

--- src/Contracts.Infra.Data/MongoDb/DataContext.cs
+++ src/Contracts.Infra.Data/MongoDb/DataContext.cs
@@ -1,6 +1,32 @@
-﻿namespace Contracts.Infra.Data.MongoDb
+﻿using System;
+using MongoDB.Driver;
+
+namespace Contracts.Infra.Data.MongoDb
 {
     public sealed class DataContext
     {
+        public static string ConnectionString { get; set; }
+        public static string DatabaseName { get; set; }
+        public static bool IsSSL { get; set; }
+
+        private IMongoDatabase _database { get; }
+
+        public DataContext()
+        {
+            try
+            {
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
+                if (IsSSL)
+                {
+                    settings.SslSettings = new SslSettings { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 };
+                }
+                var mongoClient = new MongoClient(settings);
+                _database = mongoClient.GetDatabase(DatabaseName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Não foi possível se conectar com o servidor.", ex);
+            }
+        }
     }
 }
