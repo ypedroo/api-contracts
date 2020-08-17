670aede25a355cbef029f41f95ba19a900c8fa53
ypedro <ynoa.pedro@outlook.com>
2020-08-17 19:34:22 -0300

    Adds part controller

--- src/Contracts.WebApi/Controllers/PartController.cs
+++ src/Contracts.WebApi/Controllers/PartController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Contracts.Domain.Models;
 using Contracts.Shared.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -18,5 +19,16 @@ namespace Contracts.WebApi.Controllers
         [HttpGet("info/partById")]
         public Task<IActionResult> GetPartById(int id)
             => SendCommand(new GetPartCommand(id));
+
+        //doubt if query commands are the same with different methods check thiss
+
+        [HttpGet("info/parts")]
+        public Task<IActionResult> GetParts()
+            => SendCommand(new GetAllPartsCommand());
+
+        //Refactor part to view model object on shared
+        [HttpPost("info/parts")]
+        public Task<IActionResult> CreatePart(Part part)
+            => SendCommand(new CreatePartCommand(part));
     }
 }
