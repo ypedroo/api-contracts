97d7876f22666325decf06c0db04a9cb230293cc
ypedro <ynoa.pedro@outlook.com>
2020-08-17 19:19:03 -0300

    Add controllerBase

---
+++ src/Contracts.WebApi/Controllers/ContractsControllerBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OperationResult;
+
+namespace Contracts.WebApi.Controllers
+{
+    public partial class ContractsControllerBase : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        protected ContractsControllerBase(IMediator mediator)
+            => _mediator = mediator;
+
+        protected async Task<IActionResult> SendCommand<T>(IRequest<Result<T>> request)
+            => await _mediator.Send(request).ConfigureAwait(false) switch
+            {
+                (true, var result, _) => Ok(result),
+                (_, _, var error) => HandleError(error)
+            };
+        protected async Task<IActionResult> SendCommand(IRequest<Result> request, int statusCode = 200)
+            => await _mediator.Send(request).ConfigureAwait(false) switch
+            {
+                (true, _) => StatusCode(statusCode),
+                (_, var error) => HandleError(error)
+            };
+
+        private IActionResult HandleError(Exception error)
+            => error switch
+            {
+                _ => BadRequest(new ErrorMessage(error.Message))
+            };
+    }
+}
---
+++ src/Contracts.WebApi/Controllers/ErrorMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts.WebApi.Controllers
+{
+    public sealed class ErrorMessage
+    {
+        private string Message { get; }
+
+        public ErrorMessage(string message) => Message = message;
+    }
+}
