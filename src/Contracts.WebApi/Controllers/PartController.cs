4c8608a421f7f91a1804bb188c50fe4690dfc793
ypedro <ynoa.pedro@outlook.com>
2020-08-17 19:22:37 -0300

    Add base controller for part

---
+++ src/Contracts.WebApi/Controllers/PartController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace Contracts.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class PartController : ContractsControllerBase
+    {
+        public PartController(IMediator mediator)
+        : base(mediator)
+        {
+        }
+    }
+}
